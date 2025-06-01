2708b19bfc638139aedaea0c1845e35c964c2a6d
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-06-01 12:54:28 +0100

    add match_case_calculator file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,18 +5,18 @@ operator = input("Choose the operation (+, -, *, /): ")
 match operator:
 	case "+":
 		result = num1 + num2
-		print(f"The result is {result}")
+		print(f"The result is {result}.")
 	case "-":
 		result = num1 - num2
-		print(f"The result is {result}")
+		print(f"The result is {result}.")
 	case "*":
 		result = num1 * num2
-		print(f"The result is {result}")
+		print(f"The result is {result}.")
 	case "/":
 		if num2 != 0:
 			result = num1 / num2
-			print(f"the result is {result}")
+			print(f"the result is {result}.")
 		else:
-			print("Cannot divide by zero")
+			print("Cannot divide by zero.")
 	case _:
 		print("Invalid operation selected.")
