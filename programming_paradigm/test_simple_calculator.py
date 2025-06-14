4cc61c669869be9fd1117d85c91b2da35e343ef3
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-06-15 00:16:25 +0100

    add 2 file sime_calculator and test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,17 +5,19 @@ class TestSimpleCalculator(unittest.TestCase):
 	def setUp(self):
 		self.calc = SimpleCalculator()
 	def test_addition(self):
-		self.assertEqual(self.calc.add(2,3), 5)
-		self.assertEqual(self.calc.add(10,5), 15)
+		#  self.assertEqual(self.calc.add(1, 2), 3)
+		self.assertEqual(self.calc.add(1, 2), 3)
+		self.assertEqual(self.calc.add(2, 3), 5)
+		self.assertEqual(self.calc.add(10, 5), 15)
 	def test_subtract(self):
-		self.assertEqual(self.calc.subtract(10,5), 5)
-		self.assertEqual(self.calc.subtract(5,5), 0)
+		self.assertEqual(self.calc.subtract(10, 5), 5)
+		self.assertEqual(self.calc.subtract(5, 5), 0)
 	def test_multiply(self):
-		self.assertEqual(self.calc.multiply(2,2), 4)
+		self.assertEqual(self.calc.multiply(2, 2), 4)
 		self.assertEqual(self.calc.multiply(10, 5), 50)
 	def test_divide(self):
-		self.assertEqual(self.calc.divide(10,2), 5)
-		self.assertEqual(self.calc.divide(5,5), 1)
+		self.assertEqual(self.calc.divide(10, 2), 5)
+		self.assertEqual(self.calc.divide(5, 5), 1)
 
 if __name__ == "__main__":
 	unittest.main()
