6d2fe9ef1ed78da79882b21dca1162fe4864564b
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-06-15 00:18:07 +0100

    add 2 file sime_calculator and test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,10 +9,10 @@ class TestSimpleCalculator(unittest.TestCase):
 		self.assertEqual(self.calc.add(1, 2), 3)
 		self.assertEqual(self.calc.add(2, 3), 5)
 		self.assertEqual(self.calc.add(10, 5), 15)
-	def test_subtract(self):
+	def test_subtraction(self):
 		self.assertEqual(self.calc.subtract(10, 5), 5)
 		self.assertEqual(self.calc.subtract(5, 5), 0)
-	def test_multiply(self):
+	def test_multiplication(self):
 		self.assertEqual(self.calc.multiply(2, 2), 4)
 		self.assertEqual(self.calc.multiply(10, 5), 50)
 	def test_divide(self):
