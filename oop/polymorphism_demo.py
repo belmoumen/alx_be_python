028d2f475c6a1626be7daaab2eb95b08134f5bbd
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-06-22 17:23:27 +0100

    add polymorphism demo file

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,16 @@
+class Shape:
+  def area(self):
+    raise NotImplementedError()
+
+class Rectangle(Shape):
+  def __init__(self, lenght, width):
+    self.lenght = lenght
+    self.width = width
+  def area(self):
+    return self.lenght * self.width
+
+class Circle(Shape):
+  def __init__(self, radius):
+    self.radius = radius
+  def area(self):
+    return math.pi * (self.radius**2)
