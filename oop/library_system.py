fa5798b485f3750bd36c2a086c9a69a7a3dd2c39
Mehdi Belmoumen <belmoumen.meh@gmail.com>
2025-06-22 14:44:10 +0100

    add library system file

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,7 +6,7 @@ class Book:
   def get_info(self):
     return f"Book: {self.title} by {self.author}"
 
-class Ebook(Book):
+class EBook(Book):
   def __init__(self, title, author, file_size):
     super().__init__(title, author)
     self.file_size = file_size
